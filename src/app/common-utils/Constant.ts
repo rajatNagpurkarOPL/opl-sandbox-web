dc1fd115cf4ad520c1f5c5ac060b38ccde1c0a2a
Rohit Chaudhary <rohit.chaudhary@onlinepsbloans.com>
2020-05-02 19:12:09 +0530

    login api integrated.

--- src/app/common-utils/Constant.ts
+++ src/app/common-utils/Constant.ts
@@ -1,7 +1,20 @@
 const mainPath = '/';
 export const Constant = {
     ROUTE_URL: {
-        LOGIN: mainPath + 'login'
+        LOGIN: mainPath + 'login',
+        DASHBOARD: mainPath + 'dashboard'
+    },
+    httpAndCookies: {
+        ACTK: 'tk_ac',
+        RFTK: 'tk_rc',
+        USNM: 'ur_cu',
+        LGTK: 'tk_lg',
+        COOKIES_OBJ: 'ln_ck_obj',
+        USERTYPE: 'ln_usr_type',
+        ORGID: 'ln_usr_orgId',
+        ROLEID: 'ln_usr_role_id',
+        BUSINESS_TYPE_ID: 'ln_business_type_id',
+        REST_URL: 'ln_rs_url'
     }
 
 };
---
+++ src/app/common-utils/common-services/http.service.ts
@@ -0,0 +1,117 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { CommonService } from './common.service';
+import { LoaderService } from './LoaderService';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient, private commonService: CommonService, private loaderService: LoaderService) { }
+
+  /**
+   * For Post service url with data
+   */
+  header = new HttpHeaders();
+  post(url: string, data: any, ignoreLoader?: string | string[]) {
+    if (ignoreLoader !== undefined && !ignoreLoader) {
+      this.header = this.header.append('ignoreLoader', ignoreLoader.toString());
+      return this.http.post(url, data, { headers: this.header }).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    } else {
+      this.loaderService.show();
+      return this.http.post(url, data).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    }
+  }
+
+
+  /**
+   * for get method call
+   */
+  get(url: any, responseType: any, ignoreLoader?: boolean) {
+    if (responseType === true) {
+      if (ignoreLoader !== undefined && !ignoreLoader) {
+        this.header = this.header.append('ignoreLoader', ignoreLoader.toString());
+        return this.http.get(url, { responseType: 'arraybuffer', headers: this.header }).pipe(
+          catchError((err: HttpErrorResponse) => {
+            return this.commonService.errorHandle(err);
+          }));
+      } else {
+        this.loaderService.show();
+        return this.http.get(url, { responseType: 'arraybuffer' }).pipe(
+          catchError((err: HttpErrorResponse) => {
+            return this.commonService.errorHandle(err);
+          }));
+      }
+    } else {
+      if (ignoreLoader !== undefined && !ignoreLoader) {
+        this.header = this.header.append('ignoreLoader', ignoreLoader.toString());
+        return this.http.get(url, { headers: this.header }).pipe(
+          catchError((err: HttpErrorResponse) => {
+            return this.commonService.errorHandle(err);
+          }));
+      } else {
+        this.loaderService.show();
+        return this.http.get(url).pipe(
+          catchError((err: HttpErrorResponse) => {
+            return this.commonService.errorHandle(err);
+          }));
+      }
+    }
+  }
+
+  /**
+   * for delete method call
+   */
+  delete(url: any, ignoreLoader?: string | string[]) {
+    if (ignoreLoader !== undefined && !ignoreLoader) {
+      this.header = this.header.append('ignoreLoader', ignoreLoader.toString());
+      return this.http.delete(url, { headers: this.header }).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    } else {
+      this.loaderService.show();
+      return this.http.delete(url).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    }
+  }
+
+  /**
+   * For put method call
+   */
+  put(url: string, data: any, ignoreLoader?: string | string[]) {
+    if (ignoreLoader !== undefined && !ignoreLoader) {
+      this.header = this.header.append('ignoreLoader', ignoreLoader.toString());
+      return this.http.put(url, data, { headers: this.header }).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    } else {
+      this.loaderService.show();
+      return this.http.put(url, data).pipe(
+        catchError((err: HttpErrorResponse) => {
+          return this.commonService.errorHandle(err);
+        }));
+    }
+  }
+
+  upload(url: string, formData: any) {
+    const headersData = new HttpHeaders({ 'Content-Type': 'multipart/form-data', enctype: 'multipart/form-data' });
+    return this.http.post(url, formData, { reportProgress: true, observe: 'events', headers: headersData }).pipe(
+      catchError((err: HttpErrorResponse) => {
+        return this.commonService.errorHandle(err);
+      }));
+  }
+
+}
