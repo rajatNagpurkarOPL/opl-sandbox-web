d9e66edf9991a55b1c997d21e8006dec4216068d
Rohit Chaudhary <rohit.chaudhary@onlinepsbloans.com>
2020-05-08 10:33:12 +0530

    eblr timepicker and valiations added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -50,6 +50,7 @@ import { SendBackModelService } from './common-utils/common-services/SendBackMod
     SendBackModelComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/component/settings/eblrpopup/eblrpopup.component.ts
+++ src/app/component/settings/eblrpopup/eblrpopup.component.ts
@@ -2,6 +2,8 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonService } from 'src/app/common-utils/common-services/common.service';
 import { LenderService } from 'src/app/service/lender.service';
+import * as cloneDeep from 'lodash/cloneDeep';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,10 +13,13 @@ import { LenderService } from 'src/app/service/lender.service';
 })
 export class EblrpopupComponent implements OnInit {
 
-  eblr: any = {effectiveFrom: new Date()};
+  eblr: any = { effectiveFrom: new Date() };
   eblrList = [];
+  time: any = { ampm: 'AM', hours: 12, minutes: 0 };
+  registerForm: FormGroup;
   constructor(public dialogRef: MatDialogRef<EblrpopupComponent>, public lenderService: LenderService,
-              public commonService: CommonService, @Inject(MAT_DIALOG_DATA) public data: any) { }
+              public commonService: CommonService, @Inject(MAT_DIALOG_DATA) public data: any,
+              private formBuilder: FormBuilder) { }
 
   minDate = new Date();
   // close pop up
@@ -23,39 +28,88 @@ export class EblrpopupComponent implements OnInit {
   }
 
   // save eblr
-  save(){
+  save() {
 
-    if (this.commonService.isObjectIsEmpty(this.eblr) || this.commonService.isObjectIsEmpty(this.eblr.plr) ||
-      this.commonService.isObjectIsEmpty(this.eblr.effectiveFrom)){
-        return this.commonService.warningSnackBar('Please fill required details');
-      }
-
-
-    this.eblr.plrType  = { id: 1 };
-    this.eblr.plrProdType  = { id: 3 };
-    this.eblr.plrStatus  = { id: 8 };
-    this.eblr.actionStatus  = { id: 8 };
-    this.lenderService.saveEBLR(this.eblr).subscribe(res => {
-        if (res.status === 200) {
-          this.commonService.successSnackBar(res.message);
-          this.close();
-        } else {
-          this.commonService.warningSnackBar(res.message);
-        }
-      }, (error: any) => {
-        this.commonService.errorSnackBar(error);
-      });
+    if (this.registerForm.invalid) {
+      this.commonService.warningSnackBar('Required fields are missing');
+      return false;
     }
 
+    if (this.commonService.isObjectNullOrEmpty(this.eblr) || this.commonService.isObjectNullOrEmpty(this.eblr.plr) ||
+      this.commonService.isObjectNullOrEmpty(this.eblr.effectiveFrom)) {
+      return this.commonService.warningSnackBar('Please fill required details');
+    }
+    this.eblr.plrType = { id: 1 };
+    this.eblr.plrProdType = { id: 3 };
+    this.eblr.plrStatus = { id: 8 };
+    this.eblr.actionStatus = { id: 8 };
+    this.lenderService.saveEBLR(this.eblr).subscribe(res => {
+      if (res.status === 200) {
+        this.commonService.successSnackBar(res.message);
+        this.close();
+      } else {
+        this.commonService.warningSnackBar(res.message);
+      }
+    }, (error: any) => {
+      this.commonService.errorSnackBar(error);
+    });
+  }
+
+  setAMPM() {
+    if (this.time.ampm === 'AM') {
+      this.time.ampm = 'PM';
+    } else {
+      this.time.ampm = 'AM';
+    }
+    this.setHoursMinutes();
+  }
+
+  setHoursMinutes() {
+    const copy = cloneDeep(this.eblr.effectiveFrom);
+    copy.setHours(0); copy.setMinutes(0);
+    if (this.time.ampm === 'AM'){
+      if (this.time.hours < 12){
+        copy.setHours(this.time.hours);
+      }else{
+        copy.setHours(0);
+      }
+    } else {
+      if (this.time.hours < 12){
+        copy.setHours(this.time.hours + 12);
+      }else{
+        copy.setHours(12);
+      }
+    }
+    copy.setMinutes(this.time.minutes);
+    copy.setSeconds(0);
+    this.eblr.effectiveFrom = cloneDeep(copy);
+  }
+  setTime() {
+    const eblrDate = cloneDeep(this.eblr.effectiveFrom);
+    const h = eblrDate.getHours();
+    const m = eblrDate.getMinutes();
+    this.time.ampm = h >= 12 ? 'PM' : 'AM';
+    this.time.hours = h % 12;
+    this.time.hours = h ? h : 12;
+    this.time.minutes = m < 10 ? '0' + m : m;
+    this.setHoursMinutes();
+  }
 
   ngOnInit(): void {
-    if (this.data){
+    if (this.data) {
       this.eblr = this.data;
-      this.eblr.effectiveFrom = new Date(this.eblr.effectiveFrom);
+      this.eblr.effectiveFrom =  new Date(this.eblr.effectiveFrom);
     }
+    this.setTime();
+    this.registerForm = this.formBuilder.group({
+      eblr: ['', [Validators.required, Validators.max(20)]],
+      date: ['', Validators.required],
+      hours: ['', [Validators.required, Validators.max(12), Validators.min(0)]],
+      minutes: ['', [Validators.required,  Validators.max(59), Validators.min(0)]]
+  });
 
   }
 
 }
 
-export class RadioOverviewExample {}
+export class RadioOverviewExample { }
