2b5cf918b924523b18d3dca75f9a33a59639e2ef
Rohit Chaudhary <rohit.chaudhary@onlinepsbloans.com>
2020-06-08 14:43:30 +0530

    change acc priority order drag and drop added.

--- src/app/component/products/product/account-priority-popup/account-priority-popup.component.ts
+++ src/app/component/products/product/account-priority-popup/account-priority-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-account-priority-popup',
@@ -11,11 +12,25 @@ export class AccountPriorityPopupComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data, public dialogRef: MatDialogRef<AccountPriorityPopupComponent>) { }
 
-  save(){
+  accounts = [
+    'Credit Account',
+    'Current Account',
+    'Overdraft Account',
+    'Savings Account',
+    'Other Account'
+  ];
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.accounts, event.previousIndex, event.currentIndex);
   }
-  close(d){
 
+
+  save(){
+    this.dialogRef.close({event: 'save', data: this.data});
+  }
+
+  close(){
+    this.dialogRef.close();
   }
 
   ngOnInit(): void {
