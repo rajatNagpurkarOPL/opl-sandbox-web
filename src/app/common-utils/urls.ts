127054f689b222dd928ad0d433e6472df2cfe37c
AkshayCW <akshay.patel@onlinepsbloans.com>
2021-05-05 19:17:37 +0530

    Changes for URL and Other Enhancement

--- src/app/common-utils/urls.ts
+++ src/app/common-utils/urls.ts
@@ -1,5 +1,19 @@
-const SANDBOX_BASE_URL = 'http://localhost:1104/sandbox/usermgmt';
-const ECR_BASE_URL = 'http://localhost:1103/ecr';
+const locationUrl = window.location.host.includes('sit-opl') 
+? 'https://sit-opl.instantmseloans.in' 
+: window.location.host.includes('qa-opl') 
+? 'https://qa-opl.instantmseloans.in' 
+: window.location.host.includes('uat.instant') 
+? 'https://uat.instantmseloans.in' 
+: window.location.host.includes('prepod-opl') 
+? 'https://prepod-opl.instantmseloans.in' 
+: window.location.host.includes('prod-v2') 
+? 'https://prod-v2.instantmseloans.in' 
+: window.location.host.includes('psbloansin59minutes') 
+? 'https://www.psbloansin59minutes.com' 
+: 'http://localhost:1104'; // Default is Sandbox URL
+
+const SANDBOX_BASE_URL = locationUrl + '/sandbox/usermgmt';
+const ECR_BASE_URL = locationUrl + '/gateway-service/ecr/';
 
 /**
  *  This is constants of REST URL which can be hit on backend server
