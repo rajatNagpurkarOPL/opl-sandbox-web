5586ce7e66c59131cb226154571e9627b97f1d35
AkshayCW <akshay.patel@onlinepsbloans.com>
2021-05-06 12:19:43 +0530

    URL based on the Environment

--- src/app/common-utils/urls.ts
+++ src/app/common-utils/urls.ts
@@ -1,19 +1,33 @@
-const locationUrl = window.location.host.includes('sit-opl') 
-? 'https://sit-opl.instantmseloans.in' 
-: window.location.host.includes('qa-opl') 
-? 'https://qa-opl.instantmseloans.in' 
-: window.location.host.includes('uat.instant') 
-? 'https://uat.instantmseloans.in' 
-: window.location.host.includes('prepod-opl') 
-? 'https://prepod-opl.instantmseloans.in' 
-: window.location.host.includes('prod-v2') 
-? 'https://prod-v2.instantmseloans.in' 
-: window.location.host.includes('psbloansin59minutes') 
-? 'https://www.psbloansin59minutes.com' 
-: 'http://localhost:1104'; // Default is Sandbox URL
+const SANDBOX  =  {
+    endpointFromLocal : "http://localhost:1104",
+    postFix : '/sandbox/usermgmt'
+};
 
-const SANDBOX_BASE_URL = locationUrl + '/sandbox/usermgmt';
-const ECR_BASE_URL = locationUrl + '/gateway-service/ecr/';
+const ECR  =  {
+    endpointFromLocal : "http://localhost:1101",
+    postFix : '/gateway-service/ecr'
+};
+const host = window.location.host;
+let locationUrl = host.includes('sit-opl') ? 'https://sit-opl.instantmseloans.in' 
+: host.includes('qa-opl') ? 'https://qa-opl.instantmseloans.in' 
+: host.includes('uat.instant') ? 'https://uat.instantmseloans.in' 
+: host.includes('prepod-opl') ? 'https://prepod-opl.instantmseloans.in' 
+: host.includes('prod-v2') ? 'https://prod-v2.instantmseloans.in' 
+: host.includes('psbloansin59minutes') ? 'https://www.psbloansin59minutes.com' 
+: 'http://localhost:'; // Default
+
+
+let SANDBOX_BASE_URL = '';
+let ECR_BASE_URL = ''
+if(locationUrl.includes('localhost')){
+    SANDBOX_BASE_URL = SANDBOX.endpointFromLocal + SANDBOX.postFix;
+    ECR_BASE_URL = ECR.endpointFromLocal + ECR.postFix;
+}else{
+    SANDBOX_BASE_URL = locationUrl + SANDBOX.postFix;
+    ECR_BASE_URL = locationUrl + ECR.postFix;
+}
+console.log("Sandbox Url : ",SANDBOX_BASE_URL);
+console.log("ECR Url : ",ECR_BASE_URL);
 
 /**
  *  This is constants of REST URL which can be hit on backend server
