223ea8c238a095c8437f96209691b7ee50d15ae9
abhishekopl <abhishek.mehta@onlinepsbloans.com>
2021-10-26 19:11:23 +0530

    url change!

--- src/app/common-utils/urls.ts
+++ src/app/common-utils/urls.ts
@@ -131,7 +131,7 @@ export const URLS = {
         GET_ALL_EMAIL_IDS_OF_ORGANISATION: SANDBOX_BASE_URL+ '/user/get-organisation-emails-list',
         GET_TRIGGERS_LIST: SANDBOX_BASE_URL + '/api/credits/get-triggers-list',
         SAVE_OR_UPDATE_API_TRIGGERS: SANDBOX_BASE_URL + '/api/credits/save-update-triggers',
-        GET_API_CREDIT_LOGS_LIST: SANDBOX_BASE_URL + '/api/credits/get-credit-logs-list',
+        GET_API_CREDIT_LOGS_LIST: SANDBOX_BASE_URL + '/api/credits/get-credit-logs-list-date-filter',
         DELETE_TRIGGER_BY_TRIGGER_ID: SANDBOX_BASE_URL + '/api/credits/delete-api-trigger',
         GET_CODES : SANDBOX_BASE_URL +"/master/get-mstr-codes",
         GET_API_DOCUMENTATION_DETAILS: SANDBOX_BASE_URL + '/schema/api-documentation',
