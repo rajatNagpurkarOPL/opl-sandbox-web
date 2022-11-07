fea1de46371b174dfafc734b56e9da218f6ccd34
mehulOpl <mehul.chotaliya@onlinepsbloans.com>
2022-11-07 12:31:18 +0530

    --added  the  blank Arrary validation

--- src/app/component/multiple-gst-score/multiple-gst-score.component.ts
+++ src/app/component/multiple-gst-score/multiple-gst-score.component.ts
@@ -49,14 +49,18 @@ export class MultipleGstScoreComponent implements OnInit {
   onFormSubmit() {
     console.log("this.singlegcsForm",this.multiplegcsForm);
     console.log("this.singlegcsForm.value",this.multiplegcsForm.value);
-    // if(this.multiplegcsForm.valid) {
+   //const add = this.multiplegcsForm.get('items') as FormArray;
+    if(!this.multiplegcsForm.valid)  {
+           return  this.utils.warningSnackBar("Please Enter Valid Details.");
+    }
+
+    //if(this.multiplegcsForm.valid) {
        let req :any = {};
         req.gstin=[];
       this.multiplegcsForm.value.items?.forEach(element => {
          console.log("element::",element);
          req.gstin.push(element.gstin);
       });
-
       console.log("req:: 54:::",req);
       //if(this.multiplegcsForm.valid) {
       let headerSourceEnc = this.aesGcmEncryption.encryptData(Constant.HEADER.SOURCE)
